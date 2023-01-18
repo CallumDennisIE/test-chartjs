04fd38e0ee25b255fbdccf0c96640b77bef7750d
Callum Dennis <callumdennisie@gmail.com>
2023-01-18 21:47:29 +0000

    feat: add chartJS sample code

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var xValues = ["Groceries", "Rent", "Travel", "Clothing", "Misc"];
+var yValues = [55, 49, 44, 24, 15];
+var barColors = [
+  "#b91d47",
+  "#00aba9",
+  "#2b5797",
+  "#e8c3b9",
+  "#1e7145"
+];
+
+new Chart("spending", {
+  type: "doughnut",
+  data: {
+    labels: xValues,
+    datasets: [{
+      backgroundColor: barColors,
+      data: yValues
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: "Annual Spending Breakdown 2022 - 2023"
+    }
+  }
+});
